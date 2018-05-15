3509190a85011709cd0fece58859f9df29fe0ad6
Rasmus Nilsson <zawroli@hotmail.com>
2018-05-15 10:53:00 -0700

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App content">
+        <h1 className="title is-1 has-text-black">Nilsson</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
